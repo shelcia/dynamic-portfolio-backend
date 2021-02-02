eec689332ea6d84f2e8ab0a39dfd68911e214aec
shelcia <shelcia1999@gmail.com>
2021-02-03 01:27:20 +0530

    add portfolio

--- routes/common/Common.js
+++ routes/common/Common.js
@@ -2,9 +2,17 @@ const router = require("express").Router();
 const { handleImageUpload } = require("../../middleware/imgUpload");
 const Portfolio = require("../../models/Portfolio");
 
-router.get("/portfolio/:id", async (req, res) => {
+router.get("/portfolios/:id", async (req, res) => {
   try {
     const result = await Portfolio.find({ userID: req.params.id }).exec();
+    res.status(200).send({ status: "200", message: result });
+  } catch (error) {
+    res.status(200).send({ status: "400", message: error });
+  }
+});
+router.get("/portfolio/:id", async (req, res) => {
+  try {
+    const result = await Portfolio.findById(req.params.id);
     res.status(200).send({ status: "200", message: result });
   } catch (error) {
     res.status(200).send({ status: "400", message: error });
